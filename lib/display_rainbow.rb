9ccd5436a94142f1aeab53172893a9f55ff6208d
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-05 01:09:54 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,6 @@
 def display_rainbow(letter, color)
   color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
   letter = ["R", "O", "Y", "G", "B", "I", "V"]
-  puts "#{letter[0]}: #{color[0]},"
 end
+
+  puts "#{letter[0]}: #{color[0]},"
